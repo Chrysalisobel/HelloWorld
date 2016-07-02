dc9375e7f10bf379e508c31619999486884ea5bb
Ayisha Rahman <Noraisyah-Rahman@aisyahs-workstation.local>
2016-07-02 14:13:58 +0800

    Added rounded corner for buttons.

--- HelloWorld/HelloViewController.swift
+++ HelloWorld/HelloViewController.swift
@@ -11,19 +11,25 @@ import UIKit
 class HelloViewController: UIViewController {
 
     // DECLARE VARIABLES
-    weak var greetingLabel: UILabel!
-    weak var greetButton: UIButton!
-    weak var byeButton: UIButton!
-    weak var backButton: UIButton!
+    var greetingLabel: UILabel!
+    var greetButton: UIButton!
+    var byeButton: UIButton!
+    var backButton: UIButton!
+    
+    //weak var greetingLabel: UILabel!
+    //weak var greetButton: UIButton!
+    //weak var byeButton: UIButton!
+    //weak var backButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = UIColor.yellowColor()
+        // self.view.backgroundColor = UIColor.yellowColor()
                 
         // TITLE
         let label: UILabel = UILabel(frame: CGRect(x: 16, y: 16, width: UIScreen.mainScreen().bounds.size.width - 32, height: 44))
         label.textAlignment = .Center;
             label.text = "Greetings earthlings. 👽"
+            label.backgroundColor = UIColor.whiteColor()
             label.textColor = UIColor.blackColor()
             label.font = UIFont.systemFontOfSize(25)
             self.view.addSubview(label)
@@ -36,7 +42,11 @@ class HelloViewController: UIViewController {
         greetButton.addTarget(self, action: "greet:", forControlEvents: UIControlEvents.TouchUpInside)
         self.view.addSubview(greetButton)
         self.greetButton = greetButton
-            
+        // setting rounded colour
+        greetButton.layer.cornerRadius = 5
+        greetButton.layer.borderWidth = 1
+        greetButton.layer.borderColor = UIColor.blackColor().CGColor
+        
         // BUTTON 02 - Bye
         let byeButton: UIButton = UIButton(frame: CGRect(x: CGRectGetMaxX(greetButton.frame), y: greetButton.frame.origin.y, width: greetButton.frame.size.width, height: greetButton.frame.size.height))
         byeButton.backgroundColor = UIColor.blackColor()
@@ -44,7 +54,11 @@ class HelloViewController: UIViewController {
         byeButton.addTarget(self, action: "greet:", forControlEvents: UIControlEvents.TouchUpInside)
         self.view.addSubview(byeButton)
         self.byeButton = byeButton
-            
+        // setting rounded colour
+        byeButton.layer.cornerRadius = 5
+        byeButton.layer.borderWidth = 1
+        byeButton.layer.borderColor = UIColor.blackColor().CGColor
+        
             
     }
 
