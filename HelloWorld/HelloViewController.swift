6b66d40eec333b6bb3fb706c43bbf42efd2e8393
Ayisha Rahman <Noraisyah-Rahman@aisyahs-workstation.local>
2016-07-14 19:26:00 +0800

    01- Fixed the IBOutlet for buttons 02- Added actual links 03- Added background image (not UIImage View, but Image View?)

--- HelloWorld/HelloViewController.swift
+++ HelloWorld/HelloViewController.swift
@@ -13,18 +13,15 @@ class HelloViewController: UIViewController {
     @IBOutlet weak var greetButton: UIButton!
     @IBOutlet weak var greetingLabel: UILabel!
     @IBOutlet weak var byeButton: UIButton!
-    
-    //weak var greetingLabel: UILabel!
-    //weak var greetButton: UIButton!
-    //weak var byeButton: UIButton!
-    //weak var backButton: UIButton!
+    @IBOutlet weak var danceButton: UIButton!
+    @IBOutlet weak var homeButton: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // self.view.backgroundColor = UIColor.yellowColor()
  
-        /*
-        // TITLE
+        /* TITLE (greetingButton)
+        
         let label: UILabel = UILabel(frame: CGRect(x: 16, y: 16, width: UIScreen.mainScreen().bounds.size.width - 32, height: 44))
         label.textAlignment = .Center;
             label.text = "Greetings earthlings. 👽"
@@ -33,32 +30,48 @@ class HelloViewController: UIViewController {
             label.font = UIFont.systemFontOfSize(25)
             self.view.addSubview(label)
             self.greetingLabel = label
+         */
 
-        // BUTTON 01 - Hi
+        
+        /* HI BUTTON (greetButton)..
+         
         let greetButton: UIButton = UIButton(frame: CGRect(x: 16, y: CGRectGetMaxY(label.frame), width: (UIScreen.mainScreen().bounds.size.width - 32) / 2, height: 30))
         greetButton.backgroundColor = UIColor.blackColor()
         greetButton.setTitle("Hi", forState: UIControlState.Normal)
+         */
+        
         greetButton.addTarget(self, action: "greet:", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        /* ---> ..HI BUTTON (greetButton)
+         
         self.view.addSubview(greetButton)
         self.greetButton = greetButton
         // setting rounded colour
         greetButton.layer.cornerRadius = 5
         greetButton.layer.borderWidth = 1
         greetButton.layer.borderColor = UIColor.blackColor().CGColor
+        */
         
-        // BUTTON 02 - Bye
-        let byeButton: UIButton = UIButton(frame: CGRect(x: CGRectGetMaxX(greetButton.frame), y: greetButton.frame.origin.y, width: greetButton.frame.size.width, height: greetButton.frame.size.height))
+        
+        /* BYE BUTTON (byeButton)
+        
+         let byeButton: UIButton = UIButton(frame: CGRect(x: CGRectGetMaxX(greetButton.frame), y: greetButton.frame.origin.y, width: greetButton.frame.size.width, height: greetButton.frame.size.height))
         byeButton.backgroundColor = UIColor.blackColor()
         byeButton.setTitle("Bye", forState: UIControlState.Normal)
+        */
+        
         byeButton.addTarget(self, action: "greet:", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        /* ---> ..BYE BUTTON (byeButton)
+         
         self.view.addSubview(byeButton)
         self.byeButton = byeButton
         // setting rounded colour
         byeButton.layer.cornerRadius = 5
         byeButton.layer.borderWidth = 1
         byeButton.layer.borderColor = UIColor.blackColor().CGColor
-
-       */
+       
+         */
             
     }
 
@@ -76,7 +89,24 @@ class HelloViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    // Instances Methods
+    
+    /* ---This is for experimentation with the codes below---
+     
+ func sayhi() {
+        print("Vulcan salute! 🖖🏽")
+        // Change "Greetings Earthlings." to "Vulcan salute! 🖖🏽"
+        greetingLabel.text = "Vulcan salute! 🖖🏽"
+    }
+    
+    func saybye() {
+        print("Kbye. 😒")
+        greetingLabel.text = "Kbye. 😒"
+    }
 
+    */
+    
     @IBAction func greet(sender: AnyObject) {
         if (sender as! UIButton) == self.greetButton {
             self.greetingLabel.text = "Vulcan salute! 🖖🏽"
@@ -84,4 +114,6 @@ class HelloViewController: UIViewController {
             self.greetingLabel.text = "Kbye. 😒"
         }
     }
+
+ 
 }
